3efc9efce9570bfdfb247597d23058937214aca9
Edgar Gonzalez <edgargonzalez@gmail.com>
2010-11-22 16:36:16 -0430

    initial skeleton

---
+++ src/com/hmd/tk4a/Tk4a.java
@@ -0,0 +1,15 @@
+package com.hmd.tk4a;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Tk4a extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
