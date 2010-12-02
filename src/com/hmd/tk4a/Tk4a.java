100055a40551f193dd14dd64835f8155d3eaa413
Wil Alvarez <wil_alejandro@yahoo.com>
2010-12-02 18:45:25 -0430

    Fixed bug with tabs that broke app

--- src/com/hmd/tk4a/Tk4a.java
+++ src/com/hmd/tk4a/Tk4a.java
@@ -26,32 +26,29 @@ public class Tk4a extends TabActivity {
 
         Intent intent1 = new Intent(this, FirstTabActivity.class);
         TabSpec spec1 = tabHost.newTabSpec("first");
-        //spec1.setIndicator("One", res.getDrawable(R.drawable.ic_tab_artists));
-        spec1.setIndicator("One");
+        spec1.setIndicator("One", res.getDrawable(R.drawable.ic_tab_artists));
         spec1.setContent(intent1);
-
+        
         Intent intent2 = new Intent(this, SecondTabActivity.class);
         TabSpec spec2 = tabHost.newTabSpec("second");
-        //spec2.setIndicator("Two", res.getDrawable(R.drawable.ic_tab_artists));
-        spec2.setIndicator("Two");
+        spec2.setIndicator("Two", res.getDrawable(R.drawable.ic_tab_artists));
         spec2.setContent(intent2);
         
         Intent intent3 = new Intent(this, ThirdTabActivity.class);
         TabSpec spec3 = tabHost.newTabSpec("third");
-        //spec3.setIndicator("Three", res.getDrawable(R.drawable.ic_tab_artists));
-        spec3.setIndicator("Three");
+        spec3.setIndicator("Three", res.getDrawable(R.drawable.ic_tab_artists));
         spec3.setContent(intent3);
         
         Intent intent4 = new Intent(this, FourthTabActivity.class);
         TabSpec spec4 = tabHost.newTabSpec("fourth");
-        //spec4.setIndicator("Four", res.getDrawable(R.drawable.ic_tab_artists));
-        spec4.setIndicator("Four");
+        spec4.setIndicator("Four", res.getDrawable(R.drawable.ic_tab_artists));
         spec4.setContent(intent4);
         
         tabHost.addTab(spec1);
         tabHost.addTab(spec2);
         tabHost.addTab(spec3);
         tabHost.addTab(spec4);
+        
         tabHost.setCurrentTab(0);
     }
 }
